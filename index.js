d4dc3360b6545914478c9d3ed731ed25a50febec
shalidesh <shalidesh970@gmail.com>
2022-04-25 02:10:48 +0530

    adding router  js files and testing routes using postman

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ const app = express();
 const dotenv=require("dotenv");
 const mongoose = require('mongoose');
 
+const authRoute=require("./routes/auth");
+const userRoute=require("./routes/users");
+
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.use("/",(req,res)=>{
-    console.log('this is main urllll');
-})
+// app.use("/",(req,res)=>{
+//     console.log('calling main page');
+//     res.json("calling is ok");
+
+// })
+
+app.use('/api/auth',authRoute);
+app.use('/api/users',userRoute);
 
 app.listen(5000,()=>{
-    console.log('backend conncted again');
+    console.log('port 5000');
 })
