e6885cfc900a8cb1ee82841e292b09b4bd7df35c
shalidesh <shalidesh970@gmail.com>
2022-05-02 16:42:20 +0530

    api part finished

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const mongoose = require('mongoose');
 
 const authRoute=require("./routes/auth");
 const userRoute=require("./routes/users");
+const postRoute=require("./routes/posts");
+const categoryRoute=require("./routes/categories");
+
+const multer = require("multer");
+
 
 dotenv.config();
 app.use(express.json());
@@ -19,9 +24,24 @@ mongoose
 //     res.json("calling is ok");
 
 // })
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.jpg");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use('/api/auth',authRoute);
 app.use('/api/users',userRoute);
+app.use('/api/posts',postRoute);
+app.use('/api/category',categoryRoute);
 
 app.listen(5000,()=>{
     console.log('port 5000');
